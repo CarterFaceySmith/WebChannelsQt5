25d3f92a5cb23d1c50b23046b45db089b9635658
Carter <carterfs@proton.me>
2024-08-01 09:02:08 +1000

    chore(JS): Move button function to main.js, functionality good

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ function initWebChannel(channel) {
 window.onload = function() {
     var channel = new QWebChannel(qt.webChannelTransport, initWebChannel);
 };
+
+function sendMessage() {
+    if (window.myObject) {
+        window.myObject.showAlert("Hello from HTML!");
+    } else {
+        console.log("myObject is not available yet.");
+    }
+}
